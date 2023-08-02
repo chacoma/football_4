32ac890f6a6fd905e4626d0882fa30c6c3c5acb8
chacoma <andres.chacoma@gmail.com>
2023-08-02 08:13:32 -0300

    commit

---
+++ gc0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git init
+
+git add .
+
+#excluyo la que no quiero
+#git reset -- path/to/folder/*
+
+git commit -m "commit "
+
+git branch -M main
+
+git remote add origin git@github.com:chacoma/football_4.git
+
+git push -u origin main
